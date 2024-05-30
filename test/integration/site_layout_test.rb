03de8bb0689559024b802aa857f77ec49c9420a0
添田航二郎 <soedahouse@TEC-022-0035.local>
2024-05-30 15:16:23 +0900

    Finish layout and routes

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -9,7 +9,17 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
     get contact_path
     assert_select "title", full_title("Contact")
+  end
+  test "Signup Path layout links" do
+    get signup_path
+    assert_template 'users/new'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "title", full_title("Sign up")
   end
 end
