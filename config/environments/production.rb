6643e6c0481bb83696da659916d0e960ec68fb7a
添田航二郎 <soedahouse@TEC-022-0035.local>
2024-06-12 14:35:22 +0900

    modified smtp_settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port => 587,
-    :address => 'smtp.mailgun.prg',
+    :address => 'smtp.mailgun.org',
     :user_name => ENV['MAILGUN_SMTP_LOGIN'],
     :password => ENV['MAILGUN_SMTP_PASSWORD'],
     :domain => host,
