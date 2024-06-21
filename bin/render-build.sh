0c8fce1706aa9e23982a5a3307e9b176bd35c44e
戸澤航二郎 <tozawa@tozawawatarujirounoMacBook-Air.local>
2024-06-21 23:21:43 +0900

    commentout seed

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,3 +5,4 @@ bundle install
 bundle exec rails assets:precompile RAILS_ENV=production
 bundle exec rails assets:clean RAILS_ENV=production
 bundle exec rails db:migrate RAILS_ENV=production
+# bundle exec rails db:seed RAILS_ENV=production
