677962fac8a68163cbdb74d4adec49335c36004f
戸澤航二郎 <tozawa@tozawawatarujirounoMacBook-Air.local>
2024-06-29 00:13:05 +0900

    modify text of home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
     #ページごとの完全なタイトルを返す
     def full_title(page_title = '')
-        base_title = "Ruby on Rails Tutorial Sample App"
+        base_title = "Tier list App"
         if page_title.empty?
             base_title
         else
