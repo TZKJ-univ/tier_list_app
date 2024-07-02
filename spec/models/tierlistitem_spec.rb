8523a9a550cde7fe74eeefb7eb67d9ab4a39142a
Kojiro Soeda <kojiro.soeda@techouse.jp>
2024-07-02 14:44:56 +0900

    add rspec to tierlistitem

---
+++ spec/models/tierlistitem_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe Tierlistitem, type: :model do
+  
+  it "does not allow duplicate tierlistitem names per tierlist" do
+    user = User.create(
+      name: "Aaron",
+      email: "tester@example.com",
+      password: "password",
+      password_confirmation: "password"
+    )
+
+    tierlist = user.tierlists.create(
+      list: "Tierlist 1"
+    )
+
+    tierlist.tierlistitems.create(
+      listitem: "Tierlistitem 1",
+      rank: 1
+    )
+
+    new_tierlistitem = tierlist.tierlistitems.new(
+      listitem: "Tierlistitem 1",
+      rank: 2
+    )
+
+    new_tierlistitem.valid?
+    expect(new_tierlistitem.errors[:listitem]).to include("has already been taken")
+  end
+end
