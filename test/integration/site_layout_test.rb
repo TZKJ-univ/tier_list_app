d8b0b320b689eb0d33bda51890df5377ee977e07
戸澤航二郎 <tozawa@tozawawatarujirounoMacBook-Air.local>
2024-07-02 23:04:18 +0900

    fix footer

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -30,7 +30,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_template 'users/new'
     assert_select "a[href=?]", root_path, count: 2
-    assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "title", full_title("Sign up")
   end
