f356f5d8bf680fce215d5e6a719c1c2ba2c3b266
Kojiro Soeda <kojiro.soeda@techouse.jp>
2024-07-02 18:08:56 +0900

    add trait to factory

--- spec/factories/tierlists.rb
+++ spec/factories/tierlists.rb
@@ -1,6 +1,15 @@
 FactoryBot.define do
   factory :tierlist do
-    list { "Tierlist 1" }
-    association :user    
+    sequence(:list) { |n| "Tierlist Test #{n}" }
+    created_at { Time.zone.now }
+    association :user
+
+    trait :created_yesterday do
+      created_at { Time.zone.now - 1.day }
+    end
+
+    trait :created_tommorow do
+      created_at { Time.zone.now + 1.day }
+    end
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
+  
   factory :user , aliases: [:owner] do
     name { "Aaron" }
     sequence(:email) { |n| "tester#{n}@example.com" }
     password { "password" }
     password_confirmation { "password" }
-    
   end
 end
