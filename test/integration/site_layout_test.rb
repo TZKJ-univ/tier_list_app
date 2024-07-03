077fd50e567028245aa6ba6fd5adde8834ca8264
戸澤航二郎 <tozawa@tozawawatarujirounoMacBook-Air.local>
2024-07-03 21:44:48 +0900

    fix test and view

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,36 +1,4 @@
 require "test_helper"
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-
-  def setup
-    @user = users(:michael)
-  end
-  
-  test "layout links" do
-    get root_path
-    assert_template 'static_pages/home'
-    assert_select "a[href=?]", root_path, count: 2
-    assert_select "a[href=?]", contact_path
-    assert_select "a[href=?]", signup_path
-    get contact_path
-    assert_select "title", full_title("Contact")
-  end
-
-  test "layout links with logged in" do
-    log_in_as(@user)
-    get root_path
-    assert_select "a[href=?]", users_path
-    assert_select "a[href=?]", user_path(@user)
-    assert_select "a[href=?]", edit_user_path(@user)
-    assert_select "a[href=?]", logout_path
-    assert_match @user.followers.count.to_s, response.body
-    assert_match @user.following.count.to_s, response.body
-  end
-  test "Signup Path layout links" do
-    get signup_path
-    assert_template 'users/new'
-    assert_select "a[href=?]", root_path, count: 2
-    assert_select "a[href=?]", contact_path
-    assert_select "title", full_title("Sign up")
-  end
 end
