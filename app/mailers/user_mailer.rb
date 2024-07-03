d425fe0b45093eda2389ba7ccd1421e58fb0ea56
戸澤航二郎 <tozawa@tozawawatarujirounoMacBook-Air.local>
2024-07-03 18:15:42 +0900

    fix mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,7 +7,15 @@ class UserMailer < ApplicationMailer
   #
   def account_activation(user)
     @user = user
-    mail to: user.email, subject: "Account activation"
+    mail to: user.email, subject: "アカウント有効化"
+    Rails.logger.info "Text part of the email:"
+    Rails.logger.info mail.text_part.body.decoded
+
+    # HTML部分のメール本文をログに出力
+    Rails.logger.info "HTML part of the email:"
+    Rails.logger.info mail.html_part.body.decoded
+
+    mail
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -17,6 +25,6 @@ class UserMailer < ApplicationMailer
   #
   def password_reset(user)
     @user = user
-    mail to: user.email, subject: "Password reset"
+    mail to: user.email, subject: "パスワードリセット"
   end
 end
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -5,22 +5,39 @@ class UserMailerTest < ActionMailer::TestCase
     user = users(:michael)
     user.activation_token = User.new_token
     mail = UserMailer.account_activation(user)
-    assert_equal "Account activation", mail.subject
+    assert_equal "アカウント有効化", mail.subject
     assert_equal [user.email], mail.to
     assert_equal ["tierlistgstonehill@gmail.com"], mail.from
-    assert_match user.name, mail.body.encoded
-    assert_match user.activation_token, mail.body.encoded
-    assert_match CGI.escape(user.email), mail.body.encoded
+    
+    # テキスト部分のメール本文をチェック
+    text_body = mail.text_part.body.decoded
+    assert_match user.name, text_body
+    assert_match user.activation_token, text_body
+    assert_match CGI.escape(user.email), text_body
+    
+    # HTML部分のメール本文をチェック
+    html_body = mail.html_part.body.decoded
+    assert_match user.name, html_body
+    assert_match user.activation_token, html_body
+    assert_match CGI.escape(user.email), html_body
   end
 
-  test "password reset" do
+  test "password_reset" do
     user = users(:michael)
     user.reset_token = User.new_token
     mail = UserMailer.password_reset(user)
-    assert_equal "Password reset",             mail.subject
-    assert_equal [user.email],                 mail.to
+    assert_equal "パスワードリセット", mail.subject
+    assert_equal [user.email], mail.to
     assert_equal ["tierlistgstonehill@gmail.com"], mail.from
-    assert_match user.reset_token,             mail.body.encoded
-    assert_match CGI.escape(user.email),       mail.body.encoded
+    
+    # テキスト部分のメール本文をチェック
+    text_body = mail.text_part.body.decoded
+    assert_match user.reset_token, text_body
+    assert_match CGI.escape(user.email), text_body
+    
+    # HTML部分のメール本文をチェック
+    html_body = mail.html_part.body.decoded
+    assert_match user.reset_token, html_body
+    assert_match CGI.escape(user.email), html_body
   end
 end
