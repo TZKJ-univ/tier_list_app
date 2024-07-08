dd178c7f9248cc490dcfea00be269eda2d3fe91a
Kojiro Soeda <kojiro.soeda@techouse.jp>
2024-07-08 15:23:44 +0900

    add systemspec to tierlist

--- spec/system/tierlists_spec.rb
+++ spec/system/tierlists_spec.rb
@@ -1,14 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe "Tierlists", type: :system do
-  before do
-    driven_by(:rack_test)
-  end
 
-  scenario "creating a new tierlist" do
+  scenario "creating a new tierlist", js: true do
     user = FactoryBot.create(:user)
 
     visit root_path
-    click_link ""
+    click_link "ログイン"
+    fill_in "Email", with: user.email
+    fill_in "パスワード", with: user.password
+    click_button "ログイン"
+    expect(page).to have_content "following"
+    visit root_path
+    
+
+    expect {
+      fill_in "新規Tier listを作成 ...", with: "Test List"
+      click_button "新規作成"
+
+      expect(page).to have_content "Tierlist created!"
+    }.to change(Tierlist, :count).by(1)
   end
+
+  scenario "guset access to users index" do
+    visit users_path
+    expect(page).to have_content "Please log in."
+  end
+
 end
