6988942aa6836238da03d4633a6e9b3536d55d51
戸澤航二郎 <tozawa@tozawawatarujirounoMacBook-Air.local>
2024-08-24 02:02:57 +0900

    add: delete and public and votable

--- app/models/tierlist.rb
+++ app/models/tierlist.rb
@@ -6,4 +6,14 @@ class Tierlist < ApplicationRecord
   validates :user_id, presence: true
   validates :list, presence: true, length: { maximum: 50 },
                    uniqueness: { scope: :user_id }
+  scope :public_lists, -> { where(public: true) }
+  scope :votable_lists, -> { where(votable: true) }
+
+  def votable?
+    votable
+  end
+
+  def public?
+    public
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_08_03_152758) do
+ActiveRecord::Schema[7.0].define(version: 2024_08_20_111145) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -75,6 +75,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_03_152758) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "editable_by_anyone", default: false
+    t.boolean "votable", default: true, null: false
+    t.boolean "public", default: true, null: false
     t.index ["user_id", "created_at"], name: "index_tierlists_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_tierlists_on_user_id"
   end
