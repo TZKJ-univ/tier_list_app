3e93a0390a473790dce67da559e44aa542183e63
戸澤航二郎 <tozawa@tozawawatarujirounoMacBook-Air.local>
2024-10-15 16:14:08 +0900

    modi user

--- app/models/tierlist.rb
+++ app/models/tierlist.rb
@@ -3,7 +3,6 @@ class Tierlist < ApplicationRecord
   has_many :tierlistitems, dependent: :destroy
   accepts_nested_attributes_for :tierlistitems, allow_destroy: true, reject_if: :all_blank
   default_scope -> { order(created_at: :desc) }
-  validates :user_id, presence: true
   validates :list, presence: true, length: { maximum: 50 },
                    uniqueness: { scope: :user_id }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_08_20_111145) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_15_064851) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -71,7 +71,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_20_111145) do
 
   create_table "tierlists", force: :cascade do |t|
     t.string "list"
-    t.integer "user_id", null: false
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "editable_by_anyone", default: false
